af3cf92177725be8248a0bb26c117e38b236b394
RaySSharma <ramon.sharma1@gmail.com>
2021-05-25 14:34:21 -0400

    Adding basic pipeline

---
+++ src/image_processing.py
@@ -0,0 +1,105 @@
+import pathlib
+import nibabel as nib
+import numpy as np
+import pandas as pd
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+train_dir = pathlib.Path("../data/train/")
+test_dir = pathlib.Path("../data/test/")
+patient_filename = pathlib.Path("../data/patient_classifications.csv")
+
+
+class ImageProcessing:
+    def __init__(self, desired_image_size=(150, 150)):
+        """Extracts and pre-processes images from MRI scans. Scans should be run through FSL first, and separated by train/test.
+
+        Args:
+            desired_image_size (tuple, optional): Standard output image size. Defaults to (150, 150).
+        """
+        self.desired_image_size = desired_image_size
+        self.train_dir = train_dir
+        self.test_dir = test_dir
+        self.patient_data = pd.read_csv(patient_filename)
+
+        (
+            self.X_test,
+            self.y_test,
+            self.X_train,
+            self.y_train,
+            self.X_val,
+            self.y_val,
+        ) = self.process_images()
+
+    def process_images(self):
+        test_images, test_filenames = self.processing_pipeline(self.test_dir)
+        train_images, train_filenames = self.processing_pipeline(self.train_dir)
+
+        test_class = self.get_classifications(test_filenames)
+        train_class = self.get_classifications(train_filenames)
+
+        X_train, X_val, y_train, y_val = self.split_train_test(
+            train_images, train_class
+        )
+        X_test, y_test = test_images, test_class
+
+        return X_test, to_categorical(y_test), X_train, to_categorical(y_train), X_val, to_categorical(y_val)
+
+    def get_classifications(self, filenames, cdr_threshold = 2):
+        sample_patient_id = [filename.name.split("_")[0] for filename in filenames]
+        sample_condition = np.isin(self.patient_data.Subject, sample_patient_id)
+        classifications = self.patient_data.loc[sample_condition, 'cdr'].values
+        return (classifications >= cdr_threshold)
+
+    def processing_pipeline(self, scan_dir):
+        images = []
+        scan_filenames = list(scan_dir.glob("*.nii.gz"))
+        for scan_filename in scan_filenames:
+            scan = nib.load(scan_filename).get_fdata()
+            im0, im1, im2 = self.slice_temporal_lobe(scan)
+
+            im = self.normalize_image(im1)  # keep im1 for now
+            im = self.crop_image(im)
+            images.append(im[np.newaxis])
+        images = np.vstack(images)
+        return images[:,:,:,np.newaxis], scan_filenames
+
+    def slice_temporal_lobe(self, data):
+        n_i, n_j, n_k = data.shape
+        center_i = (n_i - 1) // 2
+        center_j = (n_j - 1) // 2
+        center_k = (n_k - 1) // 2
+        slice_0 = data[center_i, :, :]
+        slice_1 = data[:, center_j, :]
+        slice_2 = data[:, :, center_k]
+        return slice_0, slice_1, slice_2
+
+    def normalize_image(self, im):
+        return im / im.max()
+
+    def split_train_test(self, X, y, test_size=0.2, random_state=440):
+        X_train, X_val, y_train, y_val = train_test_split(
+            X,
+            y,
+            test_size=test_size,
+            shuffle=True,
+            stratify=y,
+            random_state=random_state,
+        )
+        return X_train, X_val, y_train, y_val
+
+    def crop_image(self, im):
+        diff_x = im.shape[1] - self.desired_image_size[1]
+        diff_y = im.shape[0] - self.desired_image_size[0]
+        assert diff_x >= 0, "Attempting to crop a smaller image"
+        assert diff_y >= 0, "Attempting to crop a smaller image"
+
+        if not (diff_x % 2):  # even size
+            left, right = diff_x // 2, im.shape[1] - diff_x // 2
+        else:
+            left, right = diff_x // 2, (im.shape[1] - diff_x // 2) - 1
+        if not (diff_y % 2):  # even size
+            top, bottom = diff_y // 2, im.shape[0] - diff_y // 2
+        else:
+            top, bottom = diff_y // 2, (im.shape[0] - diff_y // 2) - 1
+        return im[top:bottom, left:right]
---
+++ src/network_training.py
@@ -0,0 +1,72 @@
+import numpy as np
+from pathlib import Path
+from keras import models
+from keras import layers
+from keras import losses
+from keras import metrics
+from keras.callbacks import ModelCheckpoint
+
+from image_processing import ImageProcessing
+
+model_filepath = Path("../models/")
+
+
+class craNNium:
+    def __init__(self):
+        self.data = ImageProcessing()
+
+        self.X_train = self.data.X_train
+        self.X_val = self.data.X_val
+        self.y_train = self.data.y_train
+        self.y_val = self.data.y_val
+
+        self.model = models.Sequential()
+        self.metrics = [metrics.Recall()]
+        self.set_architecture()
+        self.callback = self.compile_model()
+
+    def set_architecture(self):
+        input_shape = self.X_train.shape[1:]
+        self.model.add(
+            layers.Conv2D(16, (3, 3), activation="relu", input_shape=input_shape)
+        )
+        self.model.add(layers.MaxPooling2D((2, 2), strides=2))
+        self.model.add(layers.Conv2D(32, (3, 3), activation="relu"))
+        self.model.add(layers.MaxPooling2D((2, 2), strides=2))
+        self.model.add(layers.Conv2D(32, (3, 3), activation="relu"))
+
+        self.model.add(layers.Flatten())
+
+        self.model.add(layers.Dense(32, activation="relu"))
+        self.model.add(layers.Dense(2, activation="softmax"))
+
+        print("craNNium CNN architecture:")
+        print(self.model.summary())
+
+    def compile_model(self):
+        self.model.compile(
+            optimizer="rmsprop",
+            loss=losses.BinaryCrossentropy(from_logits=True),
+            metrics=self.metrics,
+        )
+
+        model_checkpoint_callback = ModelCheckpoint(
+            filepath=model_filepath,
+            save_weights_only=True,
+            monitor="val_{}".format(self.metrics[0].name),
+            mode="max",
+            save_best_only=True,
+        )
+        return model_checkpoint_callback
+
+    def fit(self, filename='training.model'):
+        history = self.model.fit(
+            self.X_train,
+            self.y_train,
+            epochs=10,
+            batch_size=50,
+            validation_data=(self.X_val, self.y_val),
+            callbacks=[self.callback],
+        )
+        self.history = history
+        self.model.save(model_filepath / filename)
